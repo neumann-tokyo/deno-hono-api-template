6a0ecac4dcac63dcb228f4d01c5813728e124154
kbaba1001 <kbaba1001@users.noreply.github.com>
2024-02-11 10:21:14 +0000

    feat: update pre-commit

--- hello.ts
+++ hello.ts
@@ -3,7 +3,7 @@ import { Hono } from "https://deno.land/x/hono@v3.12.8/mod.ts";
 const app = new Hono();
 
 app.get("/", (c) => {
-	return c.text("Hello Hono!");
+  return c.text("Hello Hono!");
 });
 
 Deno.serve({ port: 8080 }, app.fetch);
