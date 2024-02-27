23d1f940d2dc6a23119bba2d78395a3e1fc58f94
kbaba1001 <kbaba1001@users.noreply.github.com>
2024-02-27 22:14:51 +0000

    feat: add timezone and language setting to users table

---
+++ db/migrations/2024022701_initialize.down.sql
@@ -0,0 +1,26 @@
+begin
+;
+
+
+drop table
+  users_roles;
+
+
+drop table
+  roles_permissions;
+
+
+drop table
+  roles;
+
+
+drop table
+  permissions;
+
+
+drop table
+  users;
+
+
+commit
+;
--- src/database.ts
+++ src/database.ts
@@ -1,21 +1,16 @@
 import { Kysely, PostgresDialect } from "kysely";
 import { Pool } from "pg-pool";
-import { Database } from "./database-types.ts"; // this is the Database interface we defined earlier
+import { DB } from "./database-types.ts";
 
 const dialect = new PostgresDialect({
 	pool: new Pool({
-		database: "development",
-		host: "localhost",
-		user: "postgres",
-		port: 5432,
-		max: 10,
+		connectionString:
+			Deno.env.get("DENO_ENV") === "test"
+				? Deno.env.get("TEST_DB_URL")
+				: Deno.env.get("DATABASE_URL"),
 	}),
 });
 
-// Database interface is passed to Kysely's constructor, and from now on, Kysely
-// knows your database structure.
-// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how
-// to communicate with your database.
-export const db = new Kysely<Database>({
+export const db = new Kysely<DB>({
 	dialect,
 });
