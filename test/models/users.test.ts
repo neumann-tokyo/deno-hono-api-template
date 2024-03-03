64a68553ed43bd1b1c6db5f5cf623685386d7909
kbaba1001 <kbaba1001@users.noreply.github.com>
2024-03-03 14:04:50 +0000

    feat: use mocha

--- test/models/users.test.ts
+++ test/models/users.test.ts
@@ -1,10 +1,16 @@
+/// <reference types="npm:@types/mocha@^10.0.6" />
+
 import { assertEquals } from "std/assert/mod.ts";
 import * as uuid from "std/uuid/mod.ts";
 import { db } from "../../src/database.ts";
 import * as modelsUser from "../../src/models/users.ts";
 
-Deno.test("database", async (t) => {
-	await t.step("create", async () => {
+describe("database", () => {
+	after(async () => {
+		await db.destroy();
+	});
+
+	it("create", async () => {
 		const email = `alice-${uuid.v1.generate()}@example.com`;
 		const user = await modelsUser.create({
 			displayName: "Alice Poppins",
@@ -16,11 +22,9 @@ Deno.test("database", async (t) => {
 		assertEquals(user?.email, email);
 	});
 
-	await t.step("findById", async () => {
+	it("findById", async () => {
 		const user = await modelsUser.findById(1);
 
 		assertEquals(user?.email, "admin1@example.com");
 	});
-
-	await db.destroy();
 });
--- test/zzzz/zzzz_teardown.test.ts
+++
@@ -1,5 +0,0 @@
-import { db } from "../../src/database.ts";
-
-Deno.test("teardown", async () => {
-	await db.destroy();
-});
