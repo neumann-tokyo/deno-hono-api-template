c0b5915296b9203563c37a2f9f7975357a540578
kbaba1001 <kbaba1001@users.noreply.github.com>
2024-05-09 23:43:48 +0000

    test: add user#signIn test

--- test/models/users.test.ts
+++ test/models/users.test.ts
@@ -1,6 +1,5 @@
-/// <reference types="npm:@types/mocha@^10.0.6" />
-
-import { assertEquals } from "std/assert/mod.ts";
+import { after, describe, it } from "deno_mocha";
+import { assertEquals, assertExists } from "std/assert/mod.ts";
 import * as uuid from "std/uuid/mod.ts";
 import { db } from "../../src/database.ts";
 import * as modelsUser from "../../src/models/users.ts";
@@ -26,5 +25,21 @@ describe("database", () => {
 		const user = await modelsUser.findById(1);
 
 		assertEquals(user?.email, "admin1@example.com");
+	});
+
+	it("signIn", async () => {
+		const user = await modelsUser.signIn({
+			email: "admin1@example.com",
+			password: "password",
+		});
+
+		assertEquals(user?.email, "admin1@example.com");
+
+		const invalidUser = await modelsUser.signIn({
+			email: "admin1@example.com",
+			password: "invalid_password",
+		});
+
+		assertExists(!invalidUser);
 	});
 });
