9f68ad70beaf7e34ee7de711d7c7b25ab692cb60
kbaba1001 <kbaba1001@users.noreply.github.com>
2024-05-10 12:25:09 +0000

    feat: validation

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,23 @@
 import { Hono } from "hono/mod.ts";
+import { z } from "zod";
 import type { Users } from "../database-types.ts";
 import { generateJwtToken } from "../libs/crypto.ts";
 import * as modelUsers from "../models/users.ts";
 
 const app = new Hono();
 
+const signInSchema = z.object({
+	email: z.string(),
+	password: z.string(),
+});
 app.post("/sign-in", async (c) => {
 	const body = await c.req.json();
+	const vResult = signInSchema.safeParse(body);
+
+	if (vResult.success === false) {
+		return c.json({ message: "Invalid email or password" }, 401);
+	}
+
 	const user = await modelUsers.signIn({
 		email: body.email as string,
 		password: body.password as string,
